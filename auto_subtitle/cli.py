426b6f67d1d5e3a35b21ce33c7e8b832e0b65bb2
Ansgar <kindler.tremblay@gmail.com>
2025-08-16 13:55:28 +0200

    s

--- auto_subtitle/cli.py
+++ auto_subtitle/cli.py
@@ -62,6 +62,7 @@ def main():
         video = ffmpeg.input(path)
         audio = video.audio
         srt_path = srt_path.replace("\\", "/").replace(":", r"\:")
+        print(srt_path)
         ffmpeg.concat(
             video.filter('subtitles', srt_path, force_style="OutlineColour=&H40000000,BorderStyle=3"), audio, v=1, a=1
         ).output(out_path).run(quiet=True, overwrite_output=True)
